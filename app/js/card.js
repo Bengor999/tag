97dcedd2c1ffbbd7a562966827388fde29d789af
Bebenin Grigoriy <bengor1982@yandex.ru>
2021-11-17 14:19:53 +0300

    Первый

--- app/js/card.js
+++ app/js/card.js
@@ -1,81 +1,81 @@
-const cars = [
+const cards = [
               {
-                name: 'Машина 00',
+                name: 'chip00',
                 image: '00.jpg',
                 id: '00'
               },
               {
-                name: 'Машина 01',
+                name: 'chip01',
                 image: '01.jpg',
                 id: '01'
               },
               {
-                name: 'Машина 02',
+                name: 'chip02',
                 image: '02.jpg',
                 id: '02'
               },
               {
-                name: 'Машина 03',
+                name: 'chip03',
                 image: '03.jpg',
                 id: '03'
               },
               {
-                name: 'Машина 04',
+                name: 'chip04',
                 image: '04.jpg',
                 id: '04'
               },
               {
-                name: 'Машина 05',
+                name: 'chip05',
                 image: '05.jpg',
                 id: '05'
               },
               {
-                name: 'Машина 06',
+                name: 'chip06',
                 image: '06.jpg',
                 id: '06'
               },
               {
-                name: 'Машина 07',
+                name: 'chip07',
                 image: '07.jpg',
                 id: '07'
               },
               {
-                name: 'Машина 08',
+                name: 'chip08',
                 image: '08.jpg',
                 id: '08'
               },
               {
-                name: 'Машина 09',
+                name: 'chip09',
                 image: '09.jpg',
                 id: '09'
               },
               {
-                name: 'Машина 10',
+                name: 'chip01 10',
                 image: '10.jpg',
                 id: '10'
               },
               {
-                name: 'Машина 11',
+                name: 'chip11',
                 image: '11.jpg',
                 id: '11'
               },
               {
-                name: 'Машина 12',
+                name: 'chip12',
                 image: '12.jpg',
                 id: '12'
               },
               {
-                name: 'Машина 13',
+                name: 'chip13',
                 image: '13.jpg',
                 id: '13'
               },
               {
-                name: 'Машина 14',
+                name: 'chip14',
                 image: '14.jpg',
                 id: '14'
               },
               {
-                name: 'Машина 15',
+                name: 'chip15',
                 image: '15.jpg',
                 id: '15'
               }
